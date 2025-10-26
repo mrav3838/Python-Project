36b758b01cb29cdd337d1943280ac7cafda99a61
mrav3838 <mrav3838@gmail.com>
2025-10-26 12:30:32 +0530

    Refactor input handling and operation menu

--- main.py
+++ main.py
@@ -58,4 +58,60 @@ match o:
         print("Exiting the operation...")
         exit()
     case _:
+
         print("Invalid Choice from menu")
+
+# 1.
+def add(a, b):
+    print(a + b)
+
+# 2.
+def subtract(a, b):
+    print(a - b)
+
+# 3.
+def multiply(a, b):
+    print(a * b)
+
+# 4.
+def divide(a, b):
+    print(a / b)
+
+# 5.
+def floor_divide(a, b):
+    print(a // b)
+
+# 6.
+def modulus(a, b):
+    print(a % b)
+
+# 7.
+def power(a, b):
+    print(a ** b)
+
+# 8.
+def average(a, b):
+    print((a + b) / 2)
+
+# 9.
+def square(a, b):
+    print(a * a)
+    print(b * b)
+
+# 10.
+def absolute(a, b):
+    print(abs(a))
+    print(abs(b))
+
+# 11.
+def rounded_num(a, b):
+    print(round(a))
+    print(round(b))
+
+# 12.
+def maximum(a, b):
+    print(max(a, b))
+
+# 13.
+def minimum(a, b):
+    print(min(a, b))   
